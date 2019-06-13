cd968472d36269ef6e6f5dd9da7b15b4693deda9
PiratBolot <remedictes@gmail.com>
2019-06-13 13:10:52 +0300

    Initial commit

---
+++ src/main/java/itmo/foodtech/moneymaker/MoneymakerApplication.java
@@ -0,0 +1,13 @@
+package itmo.foodtech.moneymaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymakerApplication.class, args);
+	}
+
+}
