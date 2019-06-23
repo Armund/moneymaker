70d8dd087bad5dd13886e5b093fcc5be72ab3281
Emil Mamedov <emilmamedov99@gmail.com>
2019-06-23 23:38:45 +0300

    added field to store VK users information

---
+++ src/main/java/itmo/foodtech/moneymaker/domain/VkUser.java
@@ -0,0 +1,23 @@
+package itmo.foodtech.moneymaker.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VkUser {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String bdate;
+
+    private String city;
+
+    private int gender;
+
+    private int timezone;
+}
